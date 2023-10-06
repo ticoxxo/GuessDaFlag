890a7d66e63f3bfb8dc58dafbf0b694c18e74f26
Ticoxxo <ticoxxo@gmail.com>
2023-10-06 16:31:22 -0700

    Initial Commit

---
+++ GuessDaFlag/GuessDaFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessDaFlagApp.swift
+//  GuessDaFlag
+//
+//  Created by Alberto Almeida on 06/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessDaFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
